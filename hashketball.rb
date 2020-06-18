f42b65a72710a6c130df1af348fabb308909edac
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-18 13:45:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -169,4 +169,18 @@ def player_numbers(input)
   end
   return output
 end
+def player_stats(input)
+  game_hash.each do |team, team_info|
+    team_info.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if input == player[:player_name]
+            player.delete(:player_name) 
+            return player
+          end
+        end
+      end
+    end
+  end
+end
 # Write code here
